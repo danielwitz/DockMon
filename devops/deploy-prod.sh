b32d2a1f1e1754a367a16d955bafbec02e87d259
Nico <nicom93@gmail.com>
2018-05-06 21:02:04 +0300

    devops scripts really works

--- devops/deploy-prod.sh
+++ devops/deploy-prod.sh
@@ -3,4 +3,4 @@
 echo ${SERVER}
 ssh -i dockmon.pem ec2-user@${SERVER} "mkdir docker-monitor"
 scp -i dockmon.pem docker-compose.yml ec2-user@${SERVER}:~/docker-monitor/docker-compose.yml
-ssh -i dockmon.pem ec2-user@${SERVER} "cd docker-monitor && docker-compose up -d"
+ssh -i dockmon.pem ec2-user@${SERVER} "cd docker-monitor && docker-compose pull && docker-compose up -d"
--- devops/deploy-tester-prod.sh
+++ devops/deploy-tester-prod.sh
@@ -3,4 +3,4 @@
 echo ${SERVER}
 ssh -i dockmon.pem ec2-user@${SERVER} "mkdir docker-monitor-tester"
 scp -i dockmon.pem ../docker-monitor-tester/docker-compose.yml ec2-user@${SERVER}:~/docker-monitor-tester/docker-compose.yml
-ssh -i dockmon.pem ec2-user@${SERVER} "cd docker-monitor-tester && docker-compose up -d"
+ssh -i dockmon.pem ec2-user@${SERVER} "cd docker-monitor-tester && docker-compose pull && docker-compose up -d"
